e0d30b3c7a8bbbb51d4cdafcca26dc7519860710
Andrei Mackenzie <andreimackenzie@gmail.com>
2022-06-11 11:58:58 -0400

    Basic data store for creating randomized Autos
    Creating Autos at runtime will enable simulating eventual consistency
    between the AutoDB and the forthcoming SaleDB.

--- server/records.go
+++ server/records.go
@@ -1,6 +1,9 @@
 package server
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // A basic data model for automobiles and sale events. For the sake of the POC, assume that Autos are maintained in an OLTP database and Sales are maintained separately in an OLAP database. Costs are modeled as whole dollar amounts for simplicity.
 
@@ -8,6 +11,23 @@ type Auto struct {
 	ID        int    `json:"id"`
 	Brand     string `json:"brand"`
 	ModelName string `json:"modelName"`
+}
+
+type AutoDB struct {
+	lock  sync.Mutex
+	autos []Auto
+}
+
+func (a *AutoDB) NewRandomizedAuto() Auto {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	auto := Auto{
+		ID:        len(a.autos) + 1,
+		Brand:     randStringBytes(8),
+		ModelName: randStringBytes(8),
+	}
+	a.autos = append(a.autos, auto)
+	return auto
 }
 
 type Sale struct {
--- server/server.go
+++ server/server.go
@@ -6,11 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type server struct {
+	autoDB AutoDB
+}
+
+func (s *server) handlePostRandomizedAuto(c *gin.Context) {
+	auto := s.autoDB.NewRandomizedAuto()
+	c.JSON(http.StatusOK, auto)
+}
+
 func GetRouter() *gin.Engine {
 	r := gin.Default()
 	r.SetTrustedProxies([]string{"127.0.0.1"})
+	s := server{}
+
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{})
 	})
+	r.POST("/randomized_auto", s.handlePostRandomizedAuto)
 	return r
 }
